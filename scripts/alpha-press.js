a4893007545e7f0151b01ae9699683c5075118d1
Your NameIfranAlamBishal <you@example.comifranalam4089@gmail.com>
2024-02-17 00:43:10 +0600

    checking pressed key and current key part completed

--- scripts/alpha-press.js
+++ scripts/alpha-press.js
@@ -1,8 +1,35 @@
 function play(){
     hideScreen('home');
     showScreeen('game');
+    gameLoop();
 }
 function playAgain(){
     hideScreen('score');
     showScreeen('game');
+    gameLoop();
 }
+
+function gameLoop(){
+    const alphabet = getARandomAlphabet();
+    const shownAlphabet = document.getElementById('display-alphabet');
+    shownAlphabet.innerText = alphabet;
+   changeBackgroundColor(alphabet);
+}
+
+function checkPressedKey(event){
+    const pressedKey = event.key;
+    const currentAlphabet = document.getElementById('display-alphabet');
+    const expectedKey  = currentAlphabet.innerText.toLowerCase();
+
+    //console.log(pressedKey , expectedKey);
+
+    if(pressedKey === expectedKey){
+        console.log('Matched');
+        removeBackgroundColor(pressedKey);
+        gameLoop();
+    }
+    else{
+        console.log('failed');
+    }
+}
+document.addEventListener('keyup', checkPressedKey);
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,23 @@ function showScreeen(visible){
     const show = document.getElementById(visible);
     show.classList.remove('hidden');
 }
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+function changeBackgroundColor(elementId){
+    const alphabetId = document.getElementById(elementId);
+    alphabetId.classList.add('bg-[#FFA500]');
+}
+
+function removeBackgroundColor(elementId){
+    const alphabetId = document.getElementById(elementId);
+    alphabetId.classList.remove('bg-[#FFA500]');
+}
+
